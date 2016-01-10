077f5293b651b8ca9e90c29bf48f12a5f996a593
gunzapper <gunzapper@gmail.com>
2016-01-10 16:42:57 +0100

    I start to use set in go
    Set are an interface defined by a thrird party,on GitHub
    You can install it using
    
        $go get github.com/deckarep/golang-set

---
+++ same_of_single_dispatch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "github.com/deckarep/golang-set" // mapset -> I need to use sets
+)
+
+type Htmlize interface {
+    ToHtml() string
+}
+
+func main() {
+    //var h Htmlize
+
+    set := mapset.NewSet()
+    set.Add(1)
+    set.Add(2)
+    set.Add(3)
+
+    fmt.Println(set)
+    /*ms := MySet(set)
+    h = &ms
+
+    fmt.Println(h.ToHtml())*/
+}
+
+/*
+type MySet mapset.Set
+
+func (ms *MySet) ToHtml() string {
+  return fmt.Sprintf("<pre>%v</pre>", ms)
+}
+*/
